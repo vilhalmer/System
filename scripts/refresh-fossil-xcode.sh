a27f729570bf209004dd0761ca7fedadc7af90fb
vilhalmer <vil@vil.lv>
2016-02-28 00:44:00 -0500

    [scripts] I haven't used any of these in ages, RIP

--- scripts/refresh-fossil-xcode.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-# This script is designed to be used in conjunction with Xcode Behaviors.
-
-tmux send-keys -t "fossil:1" " clear && cd -q $XcodeWorkspacePath/../.. && ffsl" Enter
