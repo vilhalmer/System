da876ca91102cb0de8f250509bc979510ada9187
vilhalmer <vil@vil.lv>
2016-12-03 16:47:58 -0500

    [bitbar] Add wifi-tx script

---
+++ scripts/bitbar/wifi-tx.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# <bitbar.title>Wi-Fi Tx Speed</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>vilhalmer</bitbar.author>
+
+PATH=/usr/local/bin/:$PATH
+
+airport --getinfo | perl -ne 'print if s/\s+lastTxRate: ([0-9]+)$/\1 Mbps/'
