cca16330fd7494d89013aed69c88edc90816b4f4
Bill Doyle <vil@vil.lv>
2019-11-28 22:17:07 -0500

    [scripts/bar] Display active vpn connections

--- scripts/bar/network.py
+++ scripts/bar/network.py
@@ -8,10 +8,10 @@ import pydbus
 from gi.repository import GLib
 
 
-COLOR_BAD = '#f9b381aa'
-COLOR_WARN = '#ffff00aa'
-COLOR_FINE = '#ffffffaa'
-COLOR_INACTIVE = '#888888aa'
+COLOR_BAD = '#f9b381ff'
+COLOR_WARN = '#ffff00ff'
+COLOR_FINE = '#ffffffff'
+COLOR_INACTIVE = '#fffff55'
 
 
 NM_STATE_UNKNOWN = 0
@@ -33,15 +33,65 @@ def type_icon(conn_type):
         return '\uf1eb\u2009'
     elif conn_type == 'ethernet':
         return '\uf796\u2009'
+    elif conn_type == 'vpn':
+        return '\uf023\u2009'
     else:
         return '\uf6ff\u2009'
 
 
-def main():
-    bus = pydbus.SystemBus()
-    nm = bus.get('.NetworkManager')
+class NetworkStatus:
+    def __init__(self):
+        self.bus = pydbus.SystemBus()
+        self.nm = self.bus.get('.NetworkManager')
+        self.extra = []
 
-    def state_changed(state):
+        # Watch for NetworkManager restarts so the info doesn't remain stale
+        # until it gets around to reconnecting. The arguments are incorrect, so
+        # eat them.
+        self.bus.watch_name(
+            'org.freedesktop.NetworkManager',
+            name_vanished=lambda: self.state_changed(NM_STATE_UNKNOWN),
+        )
+
+        self.nm.onPropertiesChanged = self.properties_changed
+
+        # Trigger immediately for the current state.
+        self.connections_changed(self.nm.ActiveConnections)
+
+    def get(self, object_path):
+        return self.bus.get('.NetworkManager', object_path)
+
+    def properties_changed(self, properties):
+        for prop, value in properties.items():
+            if prop == 'State':
+                self.state_changed(value)
+            elif prop == 'ActiveConnections':
+                self.connections_changed(value)
+
+    def connections_changed(self, connections):
+        """
+        Look through the non-primary connections to see if any of them are
+        interesting enough to display (currently just VPN).
+        """
+        # Reset the current state in case connections have disappeared.
+        self.extra = []
+
+        for conn_path in connections:
+            if conn_path == self.nm.PrimaryConnection:
+                continue
+
+            connection = self.get(conn_path)
+            if connection.Type in ('vpn',):
+                self.extra.append(connection)
+
+        self.state_changed(self.nm.State)
+
+    def state_changed(self, state):
+        """
+        Actually draw the status item, reflecting the new state of the primary
+        connection along with any "extra" connections that we're currently
+        interested in.
+        """
         color = COLOR_BAD
         icon = type_icon('disconnected')
         text = ''
@@ -54,17 +104,13 @@ def main():
         ):
             pass
         else:
-            connection = bus.get('.NetworkManager', nm.PrimaryConnection)
-            icon = type_icon(connection.Type)
+            connection = self.get(self.nm.PrimaryConnection)
 
             # The SpecificObject has a different interface based on the
             # connection type. If it's wifi, it'll have an ssid. If not, we
             # won't display anything. The ssid is an array of bytes (yay).
             try:
-                specific_object = bus.get(
-                    '.NetworkManager',
-                    connection.SpecificObject
-                )
+                specific_object = self.get(connection.SpecificObject)
                 ssid = dbus_bytes_to_str(getattr(specific_object, 'Ssid', []))
             except KeyError:
                 # I've seen NM get into a state where the SpecificObject
@@ -80,25 +126,28 @@ def main():
             else:
                 raise ValueError("Incorrect NM_STATE")
 
+        full_text = f"{type_icon(connection.Type)}{' ' if text else ''}{text}"
+
+        for connection in self.extra:
+            # Prepend each connection, since bar item priority decreases to
+            # the left.
+            full_text = (
+                f"{type_icon(connection.Type)} {connection.Id}  " +
+                full_text
+            )
+
         print(
             json.dumps({
-                'full_text': f"{icon}{' ' if text else ''}{text}",
+                'full_text': full_text,
                 'color': color,
             }, ensure_ascii=False),
             flush=True,
         )
         time.sleep(1)  # Rate-limit changes so they look nicer.
 
-    nm.onStateChanged = state_changed
 
-    # Watch for NetworkManager restarts so the info doesn't remain stale until
-    # it gets around to reconnecting. The arguments are incorrect, so eat them.
-    bus.watch_name(
-        'org.freedesktop.NetworkManager',
-        name_vanished=lambda: state_changed(NM_STATE_UNKNOWN),
-    )
-
-    state_changed(nm.State)  # Trigger immediately for the current state.
+def main():
+    status = NetworkStatus()
     loop = GLib.MainLoop()
 
     def handle_stdin(channel, message, *data):
